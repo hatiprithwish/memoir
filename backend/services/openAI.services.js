5dc7a6f0f9f1a9e58d4ec6b9142a17d2147e834d
Prithwish Hati <hatiprithwish@gmail.com>
2024-09-30 23:19:04 +0530

    bug fix

--- backend/services/openAI.services.js
+++ backend/services/openAI.services.js
@@ -1,7 +1,8 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey:
+    "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",
 });
 
 export default openai;

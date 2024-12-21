9d904f453842e54777124539ebbf04fe009b228d
Prithwish Hati <hatiprithwish@gmail.com>
2024-12-21 13:14:04 +0530

    removed backend

--- backend/routes/gCal.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createGCalEvent } from "../controllers/gCal.controllers.js";
-
-const router = Router();
-
-router.route("/create-event").post(createGCalEvent);
-
-export default router;
--- backend/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import { ClerkExpressWithAuth } from "@clerk/clerk-sdk-node";
-import { Router } from "express";
-import { createNewUser } from "../controllers/user.controllers.js";
-
-const router = Router();
-
-router.route("/").post(ClerkExpressWithAuth(), createNewUser);
-
-export default router;
